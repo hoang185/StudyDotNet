5ab05a9fd69ed5a12d44a4795316751744822137
Hoang Vu <hoang.vu@sea-solutions.com>
2022-11-08 09:45:53 +0700

    test branch

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ public class FirstClass
         //}
         ArrayList arrList = new ArrayList(arr);
         arrList.Add(6);
-        
+        //test branch
         if (arr.Contains(6))
         {
             Console.WriteLine("arr");
